8ecfefe45e8b73a0f3bcf7afad58f8131e5337c7
Prakashandrews <prakashandrews@gmail.com>
2020-09-30 15:32:05 +0800

    UPDATE

--- DB/Connection.js
+++ DB/Connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://uvprakash:uvprakash@cluster0.5ariv.mongodb.net/dbname?retryWrites=true&w=majority";
+const uri = "mongodb+srv://uvprakash:mX44HQE2sEglZnIE@cluster0.5ariv.mongodb.net/dbname?retryWrites=true&w=majority";
 
 const connectDB = async()=> {
     await mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true } );
